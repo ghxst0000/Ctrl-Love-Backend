9427e751c10a0e8c7bffe3d9627848dce771da77
ghxst0 <lfoxy18@gmail.com>
2023-05-31 11:34:50 +0200

    add Interests

---
+++ Models/Interest.cs
@@ -0,0 +1,10 @@
+namespace CtrlLove.Models;
+
+public static class Interest
+{
+    public static readonly string VideoGames = "Video Games";
+    public static readonly string Movies = "Movies";
+    public static readonly string Coding = "Coding";
+    public static readonly string Party = "Party";
+    public static readonly string Music = "Music";
+}
