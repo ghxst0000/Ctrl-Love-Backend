97b2d1a5742e2db68ab9ac63c90231d49299a320
Losonci Blanka <losonci.blanka@mensa.hu>
2023-06-01 11:14:33 +0200

    restructure chatRoom & messages models

--- Models/MessageModel.cs
+++ Models/MessageModel.cs
@@ -5,15 +5,13 @@ public class MessageModel
     public Guid ID { get; private set; }
     public UserModel Sender { get; private set; }
     public string Body { get; set; }
-    public ChatRoomModel ChatRoom { get; private set; }
     public DateTime Timestamp { get; private set; }
 
-    public MessageModel(Guid id, UserModel sender, string body, ChatRoomModel chatRoom, DateTime timestamp)
+    public MessageModel(Guid id, UserModel sender, string body, DateTime timestamp)
     {
         ID = id;
         Sender = sender;
         Body = body;
-        ChatRoom = chatRoom;
         Timestamp = timestamp;
     }
 }
