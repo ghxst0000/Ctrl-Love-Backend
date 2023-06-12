5812cc91aa094714cab7f5b16ffd758d28c10d8b
dudasdominik <dudasdominik3@gmail.com>
2023-06-12 14:06:12 +0200

    add CtrlLoveContext.cs file

---
+++ Models/CtrlLoveContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CtrlLove.Models;
+
+public class CtrlLoveContext : DbContext
+{
+    public CtrlLoveContext(DbContextOptions<CtrlLoveContext> contextOptions) : base(contextOptions)
+    {
+        
+    }
+
+    public DbSet<ChatRoomModel> ChatRoomModels { get; set; }
+    public DbSet<MessageModel> MessageModels { get; set; }
+    public PublicUserModel PublicUserModel { get; set; }
+    public UserModel UserModel { get; set; }
+}
