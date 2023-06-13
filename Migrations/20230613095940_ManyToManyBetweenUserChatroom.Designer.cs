986abdb8db6dee09389b71271ac2ff0a04efd8e3
Le Marcell <73469368+felhos@users.noreply.github.com>
2023-06-13 12:03:16 +0200

    add new migration to reflect changes in usermodel

--- Migrations/20230613095940_ManyToManyBetweenUserChatroom.Designer.cs
+++ Migrations/20230613095940_ManyToManyBetweenUserChatroom.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CtrlLove.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CtrlLove.Migrations
 {
     [DbContext(typeof(CtrlLoveContext))]
-    partial class CtrlLoveContextModelSnapshot : ModelSnapshot
+    [Migration("20230613095940_ManyToManyBetweenUserChatroom")]
+    partial class ManyToManyBetweenUserChatroom
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230613095940_ManyToManyBetweenUserChatroom.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CtrlLove.Migrations
+{
+    /// <inheritdoc />
+    public partial class ManyToManyBetweenUserChatroom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserModel_ChatRoomModels_ChatRoomModelId",
+                table: "UserModel");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserModel_ChatRoomModelId",
+                table: "UserModel");
+
+            migrationBuilder.DropColumn(
+                name: "ChatRoomModelId",
+                table: "UserModel");
+
+            migrationBuilder.CreateTable(
+                name: "ChatRoomModelUserModel",
+                columns: table => new
+                {
+                    ChatRoomModelId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ParticipantsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatRoomModelUserModel", x => new { x.ChatRoomModelId, x.ParticipantsId });
+                    table.ForeignKey(
+                        name: "FK_ChatRoomModelUserModel_ChatRoomModels_ChatRoomModelId",
+                        column: x => x.ChatRoomModelId,
+                        principalTable: "ChatRoomModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChatRoomModelUserModel_UserModel_ParticipantsId",
+                        column: x => x.ParticipantsId,
+                        principalTable: "UserModel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatRoomModelUserModel_ParticipantsId",
+                table: "ChatRoomModelUserModel",
+                column: "ParticipantsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatRoomModelUserModel");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ChatRoomModelId",
+                table: "UserModel",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserModel_ChatRoomModelId",
+                table: "UserModel",
+                column: "ChatRoomModelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserModel_ChatRoomModels_ChatRoomModelId",
+                table: "UserModel",
+                column: "ChatRoomModelId",
+                principalTable: "ChatRoomModels",
+                principalColumn: "Id");
+        }
+    }
+}
